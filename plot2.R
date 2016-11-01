aa4c856aa8c5fa6d5f90fceff73a491a446f8fe4
Sergii Lysenko <s.lysenko@gmail.com>
2016-11-01 11:32:39 +0200

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -15,10 +15,10 @@ df$Sub_metering_1 <- as.numeric(as.character(df$Sub_metering_1))
 df$Sub_metering_2 <- as.numeric(as.character(df$Sub_metering_2))
 df$Sub_metering_3 <- as.numeric(as.character(df$Sub_metering_3))
 
-plot1 <- function() {
+plot2 <- function() {
         plot(df$timestamp,df$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
         dev.copy(png, file="plot2.png", width=480, height=480)
         dev.off()
         cat("plot2.png has been saved in", getwd())
 }
-plot1()
+plot2()
